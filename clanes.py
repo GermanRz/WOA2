7ebd11bfd960d34c3e44d2fca6aa82023bf79434
German Ramirez <germanramirez@outlook.com>
2024-10-24 14:51:54 -0500

    Modificación del metodo de listar_miembros() del la clase Clan,  muestra la fuerza total del clan y discriminado por titulos de sus miembros

--- clanes.py
+++ clanes.py
@@ -16,12 +16,39 @@ class Clan:
         self.miembros.remove(miembro)
         self.cantidadMiembros -= 1
         
+    def info_miembros(self, titulo):
+        cantidad = 0
+        fuerza, vida, defensa, ataque = 0, 0, 0, 0
+        for miembro in self.miembros:
+            if miembro.titulo == titulo:
+                cantidad += 1
+                fuerza += miembro.fuerza
+                vida += miembro.puntos_vida
+                defensa += miembro.defensa
+                ataque += miembro.ataque
+        return cantidad, fuerza, vida, defensa, ataque
+        
     def listar_miembros(self):
+        fuerzaClan, vidaClan, defensaClan, ataqueClan = 0, 0, 0, 0
         print()
         print("*** *** *** *** ***")
         print(f"The clan {self.nombre} has {self.cantidadMiembros} members")
         for miembro in self.miembros:
             print(miembro)
+            fuerzaClan += miembro.fuerza
+            vidaClan += miembro.puntos_vida
+            defensaClan += miembro.defensa
+            ataqueClan += miembro.ataque
+        cantidad, fuerza, vida, defensa, ataque = self.info_miembros("Archer")   
+        print(f"{cantidad} Archers --> fuerza({fuerza}), vida({vida}) defensa({defensa}) ataque({ataque})")
+        cantidad, fuerza, vida, defensa, ataque = self.info_miembros("Warrior")   
+        print(f"{cantidad} Warrior --> fuerza({fuerza}), vida({vida}) defensa({defensa}) ataque({ataque})")        
+        cantidad, fuerza, vida, defensa, ataque = self.info_miembros("Sorcerer")   
+        print(f"{cantidad} Sorcerer --> fuerza({fuerza}), vida({vida}) defensa({defensa}) ataque({ataque})") 
+        print()
+        cantidad, fuerza, vida, defensa, ataque = self.info_miembros("Founder")  
+        print(f"Founder {self.nombre} --> fuerza({fuerzaClan}), vida({vidaClan}) defensa({defensaClan}) ataque({ataqueClan})")  
+            
 
 #***********************************************************************
 
